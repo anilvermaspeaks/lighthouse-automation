77ab79a8fc12915e60242784ed66a7a7a68aa5a6
Anil Kumar <anikumar23@WKMZTE8AE202.global.publicisgroupe.net>
2022-04-29 09:29:50 +0530

    pupteer config

--- src/lightHouse/index.js
+++ src/lightHouse/index.js
@@ -12,6 +12,7 @@ const {appUrl, fileExtension, formFactor, categories} = config;
   const browserFlag = {chromeFlags: ['--headless']};
   const chrome =  await puppeteer.launch({
     headless:true,
+    executablePath: '/usr/bin/chromium-browser',
     ignoreDefaultArgs: ['--disable-extensions'],
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
